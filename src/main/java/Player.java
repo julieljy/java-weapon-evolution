56405b4eb2413a6142f9bf5cb320e5de08a41b43
julieljy <julieljy0328@gmail.com>
2016-01-11 11:56:21 +0800

    use playerKill to print line by line and who is the loser

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -33,25 +33,34 @@ public class Player {
         this.printer = printer;
     }
 
-    public void playerKill(Player player){
-        int minhp=player.hp;
-        for (int i=minhp;i>0;i=minhp){
-            player.hp=player.hp-this.attack;
-            if (player.hp<=0){
-                this.printer.println(player.getName()+"被打败了.");
-                break;
-            }
-            this.hp=this.hp-player.attack;
-            if (this.hp<=0){
-                this.printer.println(this.getName()+"被打败了.");
-                break;
-            }
-            minhp=player.hp;
-            if (minhp>this.hp){
-                minhp=this.hp;
-            }
-        }
+//    public void playerKill(Player player){
+//        int minhp=player.hp;
+//        for (int i=minhp;i>0;i=minhp){
+//            player.hp=player.hp-this.attack;
+//            if (player.hp<=0){
+//                this.printer.println(player.getName()+"被打败了.");
+//                break;
+//            }
+//            this.hp=this.hp-player.attack;
+//            if (this.hp<=0){
+//                this.printer.println(this.getName()+"被打败了.");
+//                break;
+//            }
+//            minhp=player.hp;
+//            if (minhp>this.hp){
+//                minhp=this.hp;
+//            }
+//        }
+//
+//    }
 
+    public void playerKill(Player player){
+        player.hp=player.hp-this.attack;
+        this.printer.println(this.getName()+"攻击了"+player.getName()+","+player.getName()+"受到了"+this.getAttack()+"点伤害,"
+                +player.getName()+"剩余生命："+player.getHp());
+        if (player.hp<=0){
+            this.printer.println(player.getName()+"被打败了");
+        }
     }
 
 }
--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,7 +1,10 @@
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InOrder;
 
 import java.io.PrintStream;
 
+import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
@@ -9,12 +12,33 @@ import static org.mockito.Mockito.verify;
  * Created by julie on 1/10/16.
  */
 public class PlayerTest {
+    private PrintStream fakedOut;
+    @Before
+    public void getFakedOut(){
+        fakedOut = mock(PrintStream.class);
+    }
     @Test
-    public void should_print_loser(){
+    public void should_print_player2_attacked_player1(){
         PrintStream fakedOut = mock(PrintStream.class);
-        Player player1 = new Player("李四",3,1,fakedOut);
-        Player player2 = new Player("张三",4,3,fakedOut);
+        InOrder inOrder = inOrder(fakedOut);
+        Player player1 = new Player("李四",20,9,fakedOut);
+        Player player2 = new Player("张三",10,8,fakedOut);
+        player2.playerKill(player1);
         player1.playerKill(player2);
-        verify(fakedOut).println("李四被打败了.");
+        player2.playerKill(player1);
+        player1.playerKill(player2);
+
+        //这个测试检测的是fakedOut的println方法传进去的参数是否相等，因此调用几次就要检测几次
+//        verify(fakedOut).println("张三攻击了李四,李四受到了8点伤害,李四剩余生命：12\n" +
+//                "    李四攻击了张三,张三受到了9点伤害,张三剩余生命：1\n" +
+//                "    张三攻击了李四,李四受到了8点伤害,李四剩余生命：4\n" +
+//                "    李四攻击了张三,张三受到了9点伤害,张三剩余生命：-8\n" +
+//                "    张三被打败了");
+
+        inOrder.verify(fakedOut).println("张三攻击了李四,李四受到了8点伤害,李四剩余生命：12");
+        inOrder.verify(fakedOut).println("李四攻击了张三,张三受到了9点伤害,张三剩余生命：1");
+        inOrder.verify(fakedOut).println("张三攻击了李四,李四受到了8点伤害,李四剩余生命：4");
+        inOrder.verify(fakedOut).println("李四攻击了张三,张三受到了9点伤害,张三剩余生命：-8");
+        inOrder.verify(fakedOut).println("张三被打败了");
     }
 }
